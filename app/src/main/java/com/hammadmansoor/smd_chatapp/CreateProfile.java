efa357507c7363a91d577177b87ad4ca91cb39bf
Hammad Mansoor <hammadmansoor75@gmail.com>
2023-10-21 21:44:16 +0500

    store user in db

--- app/src/main/java/com/hammadmansoor/smd_chatapp/CreateProfile.java
+++ app/src/main/java/com/hammadmansoor/smd_chatapp/CreateProfile.java
@@ -1,27 +1,42 @@
 package com.hammadmansoor.smd_chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.google.firebase.Firebase;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class CreateProfile extends AppCompatActivity {
-    EditText name,country,city,phoneNumber;
+    EditText name,phoneNumber;
     FirebaseAuth mAuth;
     String selectedCity, selectedCountry;
+    FirebaseDatabase database;
+    Button createProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         name = findViewById(R.id.name);
         phoneNumber = findViewById(R.id.phoneNumber);
@@ -69,12 +84,55 @@ public class CreateProfile extends AppCompatActivity {
         citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String selectedCity = (String) citySpinner.getSelectedItem();
+                selectedCity = (String) citySpinner.getSelectedItem();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
             }
         });
+
+
+        createProfile = findViewById(R.id.createProfile);
+        DatabaseReference userReference = database.getReference("user");
+        String uid = mAuth.getUid();
+
+        createProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    createProfile.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if(uid != null){
+                                User user = new User(name.getText().toString(),mAuth.getCurrentUser().getEmail(),phoneNumber.getText().toString(),selectedCountry,selectedCity);
+                                userReference.child(uid).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        startActivity(new Intent(CreateProfile.this,Home.class));
+                                    }
+                                }).addOnCanceledListener(new OnCanceledListener() {
+                                    @Override
+                                    public void onCanceled() {
+                                        Toast.makeText(CreateProfile.this,"Failed to create Profile",Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else{
+                                Toast.makeText(CreateProfile.this,"Please Create an Account",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(CreateProfile.this,signup.class));
+                            }
+                        }
+                    });
+                }
+
+        });
+
+
+
+
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/hammadmansoor/smd_chatapp/User.java
@@ -0,0 +1,53 @@
+package com.hammadmansoor.smd_chatapp;
+
+public class User {
+    String name,email,phoneNumber,country,city;
+
+    public User(String name, String email, String phoneNumber, String country, String city) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
